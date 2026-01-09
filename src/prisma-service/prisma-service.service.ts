333a9c63e7a82628dbd3b208ec67dcf04b818ed5
Fabio Squizzato <squizzato.fabio@gmail.com>
2026-01-09 17:00:02 -0300

    feat: pool conection

--- src/prisma-service/prisma-service.service.ts
+++ src/prisma-service/prisma-service.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'generated/prisma/client';
 
 @Injectable()
-export class PrismaServiceService extends PrismaClient {
+export class PrismaServiceService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const adapter = new PrismaPg({
+    const pool = new Pool({
       connectionString: process.env.DATABASE_URL,
     });
+    const adapter = new PrismaPg(pool);
     super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
   }
 }
